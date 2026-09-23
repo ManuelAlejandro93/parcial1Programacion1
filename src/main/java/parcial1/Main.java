2fe911b5f9a5627671e61104ce018226d9ff50f3
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-09-23 16:55:33 -0500

    feat: Clase Main Añadida

--- src/main/java/parcial1/Main.java
+++ src/main/java/parcial1/Main.java
@@ -2,5 +2,19 @@ package parcial1;
 
 public class Main {
     public static void main(String[] args) {
+        // Crear la empresa DevPlus con sus datos básicos
+        DevPlus empresa = new DevPlus(
+                "DevPlus",
+                "1094938205",
+                "Barrio Los Quindos M15 Casa27",
+                "3143415131",
+                "www.devplus.com");
+
+        // Crear el gestor general que coordina todo el sistema
+        GestorGeneral gestorGeneral = new GestorGeneral(empresa);
+
+        // Crear el menú y mostrar el menú principal
+        Menu menu = new Menu(gestorGeneral);
+        menu.mostrarMenuPrincipal();
     }
 }
